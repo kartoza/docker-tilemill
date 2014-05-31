f23414ccbebde0d7dd721086fad00e80fa7428db
Tim Sutton <tim@linfiniti.com>
2014-05-31 17:35:33 +0700

    Set default port for ssh to 20007

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ docker.io run \
 	--link osm-africa-postgis:osm-africa-postgis \
         -v /home/gisdata:/home/gisdata \
         -v /home/timlinux/Documents/MapBox:/Documents/MapBox \
-	-p 1100:22 \
+	-p 20007:22 \
 	-p 20008:20008 \
 	-p 20009:20009 \
 	-d \
